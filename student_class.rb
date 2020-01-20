877f17c57271f4f6079c92f89eb65bc3b9bb2627
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-20 13:50:47 +0000

    passes test_set_name

--- student_class.rb
+++ student_class.rb
@@ -5,12 +5,18 @@ class Student
     @cohort = cohort
   end
 
-  def get_name()
+  def get_name
     return @name
   end
 
   def get_cohort
     return @cohort
   end
+
+  def set_name(name)
+    @name = name
+  end
+
+
 
 end
