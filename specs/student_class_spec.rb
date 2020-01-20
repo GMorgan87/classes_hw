c30a2f13665d669e47b4ade325836e95a495c849
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-20 13:49:14 +0000

    Initila commit

---
+++ specs/student_class_spec.rb
@@ -0,0 +1,30 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../student_class')
+
+class TestStudent < Minitest::Test
+
+  def setup
+    @student = Student.new("Gareth", "E37")
+  end
+
+  def test_get_name
+    assert_equal("Gareth", @student.get_name)
+  end
+
+  def test_get_cohort
+    assert_equal("E37", @student.get_cohort)
+  end
+
+  def test_set_name
+    assert_equal("Fraser", @student.set_name("Fraser"))
+  end
+
+  # def test_set_cohort
+  #   assert_equal("E1", @student.set_cohort("E1"))
+  # end
+
+
+end
